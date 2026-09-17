e29968dbceb3b7ea8b7aec4e8a2f0bdca42dc4d5
Eric Stone <estonesan88.es@gmail.com>
2026-09-17 10:06:37 -0500

    init

---
+++ src/test/java/com/example/ericsimpleservice/EricSimpleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ericsimpleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricSimpleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
